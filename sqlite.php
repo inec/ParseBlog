70713e2a2aa182c9913c61259b3cb14d4997cd34
Y-Chi Lu <luyege+github@gmail.com>
2015-02-15 23:08:40 -0600

    sqlite

---
+++ sqlite.php
@@ -0,0 +1,23 @@
+class medoo
+<?php 
+{
+	protected $database_type = 'sqlite';
+ 
+	// For SQLite [optional]
+	protected $database_file = 'c:\xampp\database.db';
+ 
+// And get started!
+require_once 'medoo.min.php';
+ 
+$database = new medoo('c:\xampp\database.db');
+ 
+// Or via independent configuration
+$database = new medoo([
+	'database_type' => 'sqlite',
+	'database_file' => 'c:\xampp\database.db'
+]);
+ 
+$database->insert("account", [
+	"user_name" => "foo",
+	"email" => "foo@bar.com"
+]); ?>
