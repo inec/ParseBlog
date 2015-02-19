6679a47efb00f47dc3ec985825bf8792a594f321
Y-Chi Lu <luyege+github@gmail.com>
2015-02-18 21:45:37 -0600

    sql string

--- select.php
+++ select.php
@@ -6,6 +6,7 @@
          $this->open('ttt.db');
       }
    }
+
    $db = new MyDB();
    if(!$db){
       echo $db->lastErrorMsg();
@@ -13,8 +14,10 @@
       echo "Opened database successfully\n";
    }
    $sql =<<<EOF
-      UPDATE COMPANY set SALARY = 25000.00 where ID=1;
+      UPDATE COMPANY set SALARY = random() where ID=1;
 EOF;
+$sql='UPDATE COMPANY set SALARY = 2 where ID=2';
+echo $sql;
    $ret = $db->exec($sql);
    if(!$ret){
       echo $db->lastErrorMsg();
@@ -27,11 +30,15 @@ EOF;
 EOF;
    $ret = $db->query($sql);
    while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+      echo '</Br>';
       echo "ID = ". $row['ID'] . "\n";
+      echo "</br>";
       echo "NAME = ". $row['NAME'] ."\n";
+      echo '</br>';
       echo "ADDRESS = ". $row['ADDRESS'] ."\n";
+      echo "</br>";
       echo "SALARY =  ".$row['SALARY'] ."\n\n";
    }
-   echo "Operation done successfully\n";
+   echo "</br>Operation done successfully\n";
    $db->close();
 ?>
