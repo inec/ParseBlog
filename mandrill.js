130f987bf952aeb9f4548ca15305fa949ee5d2a6
Y-Chi Lu <luyege+github@gmail.com>
2015-02-24 22:14:28 -0600

    test

--- mandrill.js
+++ mandrill.js
@@ -1,46 +1,88 @@
-jQuery(function($)  
-{
-    console.log("7");
-    $("#contact_form").submit(function()
-    {
-        var email = $("#email").val(); // get email field value
-        var name = $("#fname").val()+' ' +$("#lname").val(); // get name field value
-        var msg = $("#message").val(); // get message field value
-console.log(email+name+msg);
-
-        $.ajax(
-        {
-            type: "POST",
-            url: "https://mandrillapp.com/api/1.0/messages/send.json",
-            data: {
-                'key': '2i1AwhFMxGk'+'aJ6O3Z3ousQ',
-                'message': {
-                    'from_email': email,
-                    'from_name': name,
-                    'headers': {
-                        'Reply-To': email
-                    },
-                    'subject': 'Website Contact Form Submission',
-                    'text': msg,
-                    'to': [
-                    {
-                        'email': 'luyege+pub'+'@gmail.com',
-                        'name': 'Y Lu',
-                        'type': 'to'
-                    }]
-                }
+var mandrill = require('mandrill-api/mandrill');
+var mandrill_client = new mandrill.Mandrill('MANDRILL_KEY');
+var message = {
+    "html": "<p>Example HTML content</p>",
+    "text": "Example text content",
+    "subject": "example subject",
+    "from_email": "luyege+pub@gmail.com",
+    "from_name": "Example Name",
+    "to": [{
+            "email": "luyege@gmail.com",
+            "name": "Recipient Name",
+            "type": "to"
+        }],
+    "headers": {
+        "Reply-To": "luyege+pub@gmail.com"
+    },
+    "important": false,
+    "track_opens": null,
+    "track_clicks": null,
+    "auto_text": null,
+    "auto_html": null,
+    "inline_css": null,
+    "url_strip_qs": null,
+    "preserve_recipients": null,
+    "view_content_link": null,
+    "bcc_address": "luyege+pub@gmail.com",
+    "tracking_domain": null,
+    "signing_domain": null,
+    "return_path_domain": null,
+    "merge": true,
+    "merge_language": "mailchimp",
+    "global_merge_vars": [{
+            "name": "merge1",
+            "content": "merge1 content"
+        }],
+    "merge_vars": [{
+            "rcpt": "luyege+pub2@gmail.com",
+            "vars": [{
+                    "name": "merge2",
+                    "content": "merge2 content"
+                }]
+        }],
+    "tags": [
+        "password-resets"
+    ],
+    "subaccount": "customer-123",
+    "google_analytics_domains": [
+        "example.com"
+    ],
+    "google_analytics_campaign": "luyege+pub@gmail.com",
+    "metadata": {
+        "website": "www.example.com"
+    },
+    "recipient_metadata": [{
+            "rcpt": "luyege+pub@gmail.com",
+            "values": {
+                "user_id": 123456
             }
-        })
-        .done(function(response) {
-            alert('Your message has been sent. Thank you!'); // show success message
-            $("#name").val(''); // reset field after successful submission
-            $("#email").val(''); // reset field after successful submission
-            $("#msg").val(''); // reset field after successful submission
-        })
-        .fail(function(response) {
-            alert('Error sending message.');
-        });
-        
-        return false; // prevent page refresh
-    });
+        }],
+    "attachments": [{
+            "type": "text/plain",
+            "name": "myfile.txt",
+            "content": "ZXhhbXBsZSBmaWxl"
+        }],
+    "images": [{
+            "type": "image/png",
+            "name": "IMAGECID",
+            "content": "ZXhhbXBsZSBmaWxl"
+        }]
+};
+var async = false;
+var ip_pool = "Main Pool";
+var send_at = "";
+mandrill_client.messages.send({"message": message, "async": async, "ip_pool": ip_pool, "send_at": send_at}, function(result) {
+    console.log(result);
+    /*
+    [{
+            "email": "luyege+pub@gmail.com",
+            "status": "sent",
+            "reject_reason": "hard-bounce",
+            "_id": "abc123abc123abc123abc123abc123"
+        }]
+    */
+}, function(e) {
+    // Mandrill returns the error as an object with name and message keys
+    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+    // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
 });
