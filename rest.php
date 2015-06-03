327acaa87af8a3a0a648a4c545efb07c8508f809
Y-Chi Lu <luyege+github@gmail.com>
2015-06-02 22:15:30 -0500

    path

--- rest.php
+++ rest.php
@@ -9,7 +9,7 @@ $response = \Httpful\Request::get($uri)->send();
  $_SESSION['size']='small';
 Print_r ($_SESSION['size']);
 echo "</br>";
-echo "\n 23";
+echo "\n 23user path home";
 echo $_SESSION['size']."\n123";
 //echo $response->body->sha246.'inc The Dead Weather has '.$response->status ;//. count($response->body->data) . " albums.\n";
 	?>
