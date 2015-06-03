9d01d07a769c75b27dc60256fa8592f8a2a22582
Y-Chi Lu <luyege+github@gmail.com>
2015-06-02 22:13:56 -0500

    t

--- rest.php
+++ rest.php
@@ -9,7 +9,7 @@ $response = \Httpful\Request::get($uri)->send();
  $_SESSION['size']='small';
 Print_r ($_SESSION['size']);
 echo "</br>";
-echo "\n123";
+echo "\n 23";
 echo $_SESSION['size']."\n123";
 //echo $response->body->sha246.'inc The Dead Weather has '.$response->status ;//. count($response->body->data) . " albums.\n";
 	?>
