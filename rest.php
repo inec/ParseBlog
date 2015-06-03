1ae18f70def902f9a14e8358726d143b526ca865
Y-Chi Lu <luyege+github@gmail.com>
2015-06-02 22:01:17 -0500

    codevs

--- rest.php
+++ rest.php
@@ -1,10 +1,14 @@
 <?php
 	include('./httpful.phar');
 	//
-
-$uri="http://localhost/gitphp/tes.php?method=hello&format=json";
-//$uri = "https://www.googleapis.com/freebase/v1/mqlread?query=%7B%22type%22:%22/music/artist%22%2C%22name%22:%22The%20Dead%20Weather%22%2C%22album%22:%5B%5D%7D";
+session_start();
+$uri="http://localhost/ghpblog/ParseBlog/tes.php?method=hello&format=json";
+$uri = "https://www.googleapis.com/freebase/v1/mqlread?query=%7B%22type%22:%22/music/artist%22%2C%22name%22:%22The%20Dead%20Weather%22%2C%22album%22:%5B%5D%7D";
 $response = \Httpful\Request::get($uri)->send();
- 
-echo $response->body->sha246.'inc The Dead Weather has '.$response->status ;//. count($response->body->data) . " albums.\n";
+ echo 'inc The Dead Weather has '.count($response->body->result->album) . " albums.\n";
+ $_SESSION['size']='small';
+Print_r ($_SESSION['size']);
+echo "</br>";
+echo $_SESSION['size']."\n123";
+//echo $response->body->sha246.'inc The Dead Weather has '.$response->status ;//. count($response->body->data) . " albums.\n";
 	?>
