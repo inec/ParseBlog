a6bbe3d4efa3cb824460c6c21044c2fe386f95a9
Y-Chi Lu <luyege+github@gmail.com>
2015-06-02 22:29:54 -0500

    session

--- rest.php
+++ rest.php
@@ -1,15 +1,22 @@
 <?php
 	include('./httpful.phar');
 	//
-session_start();
+if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    } 
 $uri="http://localhost/ghpblog/ParseBlog/tes.php?method=hello&format=json";
 $uri = "https://www.googleapis.com/freebase/v1/mqlread?query=%7B%22type%22:%22/music/artist%22%2C%22name%22:%22The%20Dead%20Weather%22%2C%22album%22:%5B%5D%7D";
 $response = \Httpful\Request::get($uri)->send();
- echo 'inc The Dead Weather has '.count($response->body->result->album) . " albums.\n";
+//$temp=$response->body->result->album;
+
+echo 'inc The Dead Weather '.count($response->body->result->album) . " albums.\n";
  $_SESSION['size']='small';
 Print_r ($_SESSION['size']);
 echo "</br>";
 echo "\n 23user path home aggin";
 echo $_SESSION['size']."\n123";
+echo "<hr/>session id </br>". session_id()."<hr/>";
+//echo $temp[1]."=",	$temp[12];
 //echo $response->body->sha246.'inc The Dead Weather has '.$response->status ;//. count($response->body->data) . " albums.\n";
 	?>
