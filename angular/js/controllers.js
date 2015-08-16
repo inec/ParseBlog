d511164ef222d273832c070ae71f75ae2e1af513
Y-Chi Lu <luyege+github@gmail.com>
2015-08-15 22:26:56 -0500

    up and run angular

---
+++ angular/js/controllers.js
@@ -0,0 +1,10 @@
+var myApp=angular.module('myApp',[]);
+myApp.controller("MyController",function MyController($scope){
+
+$scope.author={
+    'name' : 'R V',
+    'title' : 'S A',
+    'company' : 'L.com'
+}
+
+});
