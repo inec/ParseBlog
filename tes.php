02d5c37088b783ad4d82c86e2c88f417d0964da1
Y-Chi Lu <luyege+github@gmail.com>
2015-08-22 11:30:17 -0500

    restful json php

--- tes.php
+++ tes.php
@@ -23,12 +23,7 @@ credit : I forgot
 
  //aLEoK5HeLAVMNmKcuN1EfxLwltPjxYeXjcIkhERjNIM=
 // aa86afc9521e9342fecab5aaf0bee48ad9beec89
-/**
- * Deliver HTTP Response
- * @param string $format The desired HTTP response content type: [json, html, xml]
- * @param string $api_response The desired HTTP response data
- * @return void
- **/
+
 function deliver_response($format, $api_response){
  
     // Define HTTP responses
@@ -146,7 +141,7 @@ if( $authentication_required ){
     }
  
 }
- 
+
 // --- Step 3: Process Request
  
 // Method A: Say Hello to the API
@@ -154,7 +149,7 @@ if( strcasecmp($_GET['method'],'hello') == 0){
     $response['code'] = 1;
     $response['status'] = $api_response_code[ $response['code'] ]['HTTP Response'];
 
-    $response['data'] = 'ello World-'.$_GET['method'].'-';
+    $response['data'] = 'Data World-'.$_GET['method'].'-';
 
     $sha= hash("Sha256",     "The quick brown fox jumped over the lazy dog.",true);
 $sha2=base64_encode($sha);
@@ -164,9 +159,29 @@ $iscurl=function_exists('curl_version');
 $response['is_curl']=$iscurl;
 }
  
+if( strcasecmp($_GET['method'],'status') == 0  ){
+    $response['code'] = 1;
+    $response['status'] = $api_response_code[ $response['code'] ]['HTTP Response'];
+
+    $response['data'] = 'stat World-'.$_GET['method'].'-';
+ $response['no'] = 'stat World-'.$_GET['no'].'-';
+
+
+
+}
+ 
+ 
+ 
 // --- Step 4: Deliver Response
  
 // Return Response to browser
+
+if (isset($_REQUEST['format']))
+{
 deliver_response($_GET['format'], $response);
- 
+ }
+ else
+ {
+ deliver_response('json', $response);
+ }
 ?>
